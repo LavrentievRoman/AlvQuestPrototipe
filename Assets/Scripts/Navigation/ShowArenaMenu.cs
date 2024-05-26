707561e3d2266545258bdbfac13dff3d495e6418
Danilich <danilakop03@gmail.com>
2024-05-26 15:48:17 +0700

    merged arena and main

---
+++ Assets/Scripts/Navigation/ShowArenaMenu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowArenaMenu : MonoBehaviour
+{
+
+    [SerializeField] private GameObject arenaMenu;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button button = GetComponent<Button>();
+        button.onClick.AddListener(() => {
+            arenaMenu.SetActive(true);
+            transform.parent.gameObject.SetActive(false);});
+    }
+}
