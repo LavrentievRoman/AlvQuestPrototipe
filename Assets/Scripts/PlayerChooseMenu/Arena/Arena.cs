322bb9b2d65ea2953964c008c475fc0f516a9bda
Danilich <danilakop03@gmail.com>
2024-05-27 11:51:37 +0700

    добавил одну строчку кода

--- Assets/Scripts/PlayerChooseMenu/Arena/Arena.cs
+++ Assets/Scripts/PlayerChooseMenu/Arena/Arena.cs
@@ -1,5 +1,7 @@
+using AlvQuest_Editor;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -7,6 +9,7 @@ public class Arena : MonoBehaviour
 {
 
     [SerializeField] private GameObject namePanel;
+    [SerializeField] private GameObject characteristicPanel;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,6 +21,9 @@ public class Arena : MonoBehaviour
         if (CharacterDataBase.Instance.SelectedCharacter is CharacterDTO character)
         {
             namePanel.GetComponent<Text>().text = character.BaseData.Name;
+
+            GameObject strengthPanel = characteristicPanel.transform.Find("Strength").gameObject;
+            strengthPanel.transform.Find("ParameterValue").gameObject.GetComponent<Text>().text = character.Characteristics[ECharacteristic.Strength].ToString();
         }
         
     }
